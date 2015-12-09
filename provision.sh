4b4d2a5a759727dfae8aeebdf82269278a3ca2db
gomesuit <gomesuit@gmail.com>
2015-12-09 11:40:46 +0900

    cassandra

--- provision.sh
+++ provision.sh
@@ -10,6 +10,10 @@ echo "192.168.33.13 cassandra2" >> /etc/hosts
 # git clone provisioner
 cd /home/vagrant
 git clone https://github.com/gomesuit/provisioner.git
+
 # run ansbile
 cd /home/vagrant/provisioner
 ansible-playbook -i hosts provisioner.yml
+
+# set ansible
+#cat /vagrant/hosts > /etc/ansible/hosts
